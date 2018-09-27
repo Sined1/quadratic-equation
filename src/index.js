89f655e379d6cc60482bb266ee9e1ae3434a78ea
Denis Babuskin <3169477@gmail.com>
2018-09-27 20:56:45 +0300

    solve quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let ar = equation.split(' ');
+  let a, b, c;
+
+  //definition a, b and c for further equation
+  a = parseInt(ar[0]);
+  if (ar[3] == '-') {
+    b = parseInt(ar[3] + ar[4]);
+  } else b = parseInt(ar[4]);
+  if (ar[7] == '-') {
+    c = parseInt(ar[7] + ar[8])
+  } else c = parseInt(ar[8]);
+
+  let d = b * b - 4 * a * c;
+  let x1 = Math.round((-b + Math.sqrt(d)) / 2 / a);
+  let x2 = Math.round((-b - Math.sqrt(d)) / 2 / a);
+  return [x1, x2];
 }
